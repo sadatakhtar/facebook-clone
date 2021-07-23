1df34822efd40fac2a169d9bf81334a11294d25a
sadatakhtar <sadat0akhtar@gmail.com>
2021-07-23 21:04:30 +0100

    created header center

--- components/Header.js
+++ components/Header.js
@@ -13,12 +13,13 @@ import {
   SearchIcon, 
   ShoppingCartIcon,
 } from '@heroicons/react/outline';
+import HeaderIcon from './HeaderIcon';
 
 function Header() {
   return (
-    <div className="flex items-center">
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
       {/* left */}
-      <div>
+      <div className='flex items-center'>
         <Image 
         src="https://links.papareact.com/5me" 
         width={40} 
@@ -26,13 +27,22 @@ function Header() {
         layout="fixed"
         />
       </div>
-      <div className="flex ml-2 items-center rounded-full">
+      <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
         <SearchIcon className='h-6 text-gray-600'/>
-        <input type="text" placeholder="Search Facebook"/>
+        <input className='flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink' type="text" placeholder="Search Facebook"/>
       </div>
 
 
       {/* center */}
+      <div className='flex justify-center flex-grow'>
+        <div className='flex space-x-6 md:space-x-2'>
+          <HeaderIcon Icon={HomeIcon}/>
+          <HeaderIcon Icon={FlagIcon}/>
+          <HeaderIcon Icon={PlayIcon}/>
+          <HeaderIcon Icon={ShoppingCartIcon}/>
+          <HeaderIcon Icon={UserGroupIcon}/>
+        </div>
+      </div>
 
       {/* right */}
       
