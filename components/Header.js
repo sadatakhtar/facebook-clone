ad62629754f324408b29bcd0e07f374fc335f7c8
sadatakhtar <sadat0akhtar@gmail.com>
2021-07-23 21:11:45 +0100

    added active prop to home icon

--- components/Header.js
+++ components/Header.js
@@ -36,7 +36,7 @@ function Header() {
       {/* center */}
       <div className='flex justify-center flex-grow'>
         <div className='flex space-x-6 md:space-x-2'>
-          <HeaderIcon Icon={HomeIcon}/>
+          <HeaderIcon active Icon={HomeIcon}/>
           <HeaderIcon Icon={FlagIcon}/>
           <HeaderIcon Icon={PlayIcon}/>
           <HeaderIcon Icon={ShoppingCartIcon}/>
