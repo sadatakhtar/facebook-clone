7f44ae14879dfb0dc1c986b4a90cc3f3c60932d3
sadatakhtar <sadat0akhtar@gmail.com>
2021-07-24 14:51:36 +0100

    created secure session with nextauth to login to facebook

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import { getSession } from 'next-auth/client'
 import Head from 'next/head'
 import Header from '../components/Header'
+import Login from '../components/Login'
 
-export default function Home() {
+export default function Home({ session}) {
+  if(!session){
+    return <Login />
+
+  };
   return (
     <div>
       <Head>
@@ -19,4 +25,14 @@ export default function Home() {
      
     </div>
   )
+}
+
+export async function getServerSideProps(context){
+  const session = await getSession(context);
+  return {
+    props: {
+      session
+    }
+  }
+
 }
