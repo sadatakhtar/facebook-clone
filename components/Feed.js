0565970da7d6d00f091ac333b7def1148413477b
sadatakhtar <sadat0akhtar@gmail.com>
2021-07-26 15:29:42 +0100

    created posts, pulled data from firebase storage and displayed on screen as post with image

--- components/Feed.js
+++ components/Feed.js
@@ -1,5 +1,6 @@
 import Stories from "./Stories"
 import InputBox from "./InputBox"
+import Posts from './Posts'
 
 function Feed() {
   return (
@@ -9,6 +10,7 @@ function Feed() {
         <Stories />
         <InputBox />
         {/* posts */}
+        <Posts />
       </div>
     </div>
   )
---
+++ components/Post.js
@@ -0,0 +1,46 @@
+import Image from 'next/image'
+import { ChatAltIcon, ShareIcon, ThumbUpIcon} from '@heroicons/react/outline'
+
+function Post({ name, message, email, postImage, image, timestamp}) {
+  return (
+    <div className='flex flex-col'>
+      <div className='bg-white p-5 mt-5 rounded-t-2xl shadow-sm'>
+        <div className='flex items-center space-x-2'>
+          <img className='rounded-full' src={image} width={40} height={40} alt=''/>
+          <div>
+            <p className='font-medium'>{name}</p>
+            <p className='text-xs text-gray-400'>
+              {new Date(timestamp?.toDate()).toLocaleString()}</p>
+          </div>
+        </div>
+        <p className='pt-4'>{message}</p>
+      </div>
+      {postImage && (
+        <div className='relative h-56 md:h-96 bg-white'>
+          <Image src={postImage} objectFit='cover' layout='fill'/>
+        </div>
+      )}
+
+      <div className='flex justify-evenly items-center bg-white rounded-b-2xl 
+      shadow-md text-gray-400 border-t'>
+        <div className=' inputIcon rounded-none rounded-bl-2xl'>
+          <ThumbUpIcon className='h-4'/>
+          <p className='text-xs sm:text-base' >Like</p>
+        </div>
+        <div className=' inputIcon rounded-none '>
+          <ChatAltIcon className='h-4'/>
+          <p className='text-xs sm:text-base'>Comment</p>
+        </div>
+        <div className=' inputIcon rounded-none rounded-br-2xl'>
+          <ShareIcon className='h-4'/>
+          <p className='text-xs sm:text-base'>Share</p>
+        </div>
+
+      </div>
+
+      
+    </div>
+  )
+}
+
+export default Post
